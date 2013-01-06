e39f9c23147dce0032d0a5b811d9bae6ae491096
Jyri Tuulos <jyri@bitdeli.com>
2013-01-06 14:38:43 -0800

    add package and snippet skeleton, add Makefile

---
+++ src/tracking.js
@@ -0,0 +1,15 @@
+/*!
+  * Bitdeli JavaScript tracking library
+  * Copyright (c) 2012 Bitdeli Inc
+  * https://github.com/bitdeli/bitdeli-tracking-js
+  * MIT license
+  */
+!function (context) {
+
+    // Get async command queue (defined in tracking snippet)
+    var _bdq = context["_bdq"] || [];
+
+    // Set library version
+    _bdq["__LV"] = "0.0.1";
+
+}(this);
