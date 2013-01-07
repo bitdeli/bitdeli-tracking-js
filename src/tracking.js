675c8030548ed8efe26a96bb916f807dec69beb0
Jyri Tuulos <jyri@bitdeli.com>
2013-01-06 23:49:50 -0800

    use random UUID by default for $uid property

--- src/tracking.js
+++ src/tracking.js
@@ -132,6 +132,9 @@ _.extend(Bitdeli.Cookie.prototype, {
 
     initialize: function(options) {
         this.load();
+        this.setOnce({
+            $uid: _.UUID.v4()
+        });
     },
 
     load: function() {
@@ -330,6 +333,89 @@ _.cookie = (function (document, undefined) {
     return cookie;
 
 })(document);
+
+
+// UUID generation based on uuid.js
+// https://github.com/broofa/node-uuid
+// Copyright (c) 2010-2012 Robert Kieffer
+// MIT License - http://opensource.org/licenses/mit-license.php
+_.UUID = (function() {
+    var _global = context;
+    var _rng;
+
+    if (!_rng && _global.crypto && crypto.getRandomValues) {
+        var _rnds8 = new Uint8Array(16);
+        _rng = function whatwgRNG() {
+            crypto.getRandomValues(_rnds8);
+            return _rnds8;
+        };
+    }
+
+    if (!_rng) {
+        var  _rnds = new Array(16);
+        _rng = function() {
+            for (var i = 0, r; i < 16; i++) {
+                if ((i & 0x03) === 0) r = Math.random() * 0x100000000;
+                _rnds[i] = r >>> ((i & 0x03) << 3) & 0xff;
+            }
+            return _rnds;
+        };
+    }
+
+    // Buffer class to use
+    var BufferClass = typeof(Buffer) == 'function' ? Buffer : Array;
+
+    // Map for number -> hex string conversion
+    var _byteToHex = [];
+    for (var i = 0; i < 256; i++) {
+        _byteToHex[i] = (i + 0x100).toString(16).substr(1);
+    }
+
+    // **`unparse()` - Convert UUID byte array (ala parse()) into a string**
+    function unparse(buf, offset) {
+        var i = offset || 0, bth = _byteToHex;
+        return  bth[buf[i++]] + bth[buf[i++]] +
+                bth[buf[i++]] + bth[buf[i++]] + '-' +
+                bth[buf[i++]] + bth[buf[i++]] + '-' +
+                bth[buf[i++]] + bth[buf[i++]] + '-' +
+                bth[buf[i++]] + bth[buf[i++]] + '-' +
+                bth[buf[i++]] + bth[buf[i++]] +
+                bth[buf[i++]] + bth[buf[i++]] +
+                bth[buf[i++]] + bth[buf[i++]];
+    }
+
+    // **`v4()` - Generate random UUID**
+    // See https://github.com/broofa/node-uuid for API details
+    function v4(options, buf, offset) {
+        // Deprecated - 'format' argument, as supported in v1.2
+        var i = buf && offset || 0;
+
+        if (typeof(options) == 'string') {
+            buf = options == 'binary' ? new BufferClass(16) : null;
+            options = null;
+        }
+        options = options || {};
+
+        var rnds = options.random || (options.rng || _rng)();
+
+        // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
+        rnds[6] = (rnds[6] & 0x0f) | 0x40;
+        rnds[8] = (rnds[8] & 0x3f) | 0x80;
+
+        // Copy bytes to buffer, if provided
+        if (buf) {
+            for (var ii = 0; ii < 16; ii++) {
+                buf[i + ii] = rnds[ii];
+            }
+        }
+
+        return buf || unparse(rnds);
+    }
+
+    // Export public API
+    return { v4: v4 };
+
+})();
 
 
 // Bitdeli tracking library entry point
